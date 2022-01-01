9f9f79a85b71cdbffbe6f1220c16c638853a9d58
salsabilmislat <salsabilmislat196@gmail.com>
2022-01-01 18:26:28 +0200

    doctors from backend

--- components/doctors.js
+++ components/doctors.js
@@ -1,19 +1,71 @@
 import axios from 'axios'
 import React from 'react'
+import { useState, useEffect } from 'react';
 
 function Doctors() {
-    const config ={
-        method: "GET",
-        url: "http://127.0.0.1:8000/api/v1/doctor/",
+    const [token, setToken] = useState('');
+    const [doctorData, setDoctorData] = useState([]);
+    useEffect(() => {
+        setToken('hi');
+        const config = {
+            method: "POST",
+            url: "http://127.0.0.1:8000/auth/login/",
+            data: {
+                "username": "3",
+                "password": "3"
+            }
+
+        }
+        axios(config).then(data => {
+            setToken(data.data.access);
+            // console.log(data.data.access);
+        });
+        console.log(token)
+
+        handel();
+    }, []);
+
+    const handel = () => {
+        const config = {
+            method: "GET",
+            url: "http://127.0.0.1:8000/api/v1/doctor/",
+            headers: { 'Authorization': 'Bearer ' + token },
+
+        }
+        axios(config).then(res => {
+            setDoctorData(res.data);
+            console.log(res.data);
+
+        });
+        console.log(doctorData);
+
     }
-    axios(config).then(res => {
-        console.log(res.data);
-    })
+
     return (
         <div>
+            {
+                doctorData.map((doctor) => {
+                    return (
+                        <>
+                            <div class="card">
+                                <div class="container">
+                                    <h4><b>{doctor.name}</b></h4>
+                                    <p>Adreess : {doctor.city}, {doctor.town} , {doctor.building_number} , {doctor.street}</p>
+                                    <button>Book Appointment</button>
+                                </div>
+                            </div>
+
+
+                        </>
+                    )
+                })
+            }
+
             
         </div>
+
     )
 }
 
 export default Doctors
+
--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,8 @@ export default function Home() {
       </Head> */}
 
       <Register />
-      <Doctors/>
+      <Doctors />
+
     </div>
   )
 }
