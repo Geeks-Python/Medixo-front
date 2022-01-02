91f03c151dcff5215e89b69dfdcf2c0fde7ccbd4
rihan <Rihanfoudeh@gmail.com>
2022-01-02 10:33:54 +0200

    Login and regester

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '../styles/globals.css'
 
+import '../styles/loginPageContainer.css'
+
+import '../styles/patientRegisterPage.css'
+
+import '../styles/doctorRegisterPage.css'
+
+
+
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,52 @@
 import Head from 'next/head'
-import Register from '../components/register'
+import Register from '../components/register/register'
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Link from 'next/link'
 
+import { useState } from 'react';
+
+
+
+// import Login from '../components/loginPage/LoginPageContainer'
+import Login from '../components/loginPage/Login'
+import PatientRegisterPage from './PatientRegisterPage'
+
+import DoctorRegisterPage from './DoctorRegisterPage'
+
+
+import axios from 'axios';
+
+
+
+const baseUrl ='http://127.0.0.1:8000/';
+const tokenUrl = baseUrl+'api/token/';
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      {/* <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head> */}
+  
+  const [token, setToken] = useState('');
 
-      <Register />
-    </div>
+  const submitHandler = async (e, credintials)=>{
+    e.preventDefault();
+    axios.post(tokenUrl,credintials).then(data=>{
+      setToken(data.data.access)
+    });
+    console.log(token)
+  }
+
+  if (!token)  return <Login submitHandler={submitHandler}/> 
+  return (
+
+    
+    <main className="App">
+      <div className="right-container">
+
+        <DoctorRegisterPage token={token} />
+
+
+      </div>
+
+
+    </main>
+
+
   )
 }
